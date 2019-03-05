945bca2b18d6ae26de4a8ddc70cccbae77e0d4e3
frank <frank.code@outlook.com>
2019-03-05 15:53:13 +0800

    修改定位代码

--- miniprogram/pages/mapDemo/mapDemo.js
+++ miniprogram/pages/mapDemo/mapDemo.js
@@ -1,8 +1,24 @@
 Page({
+  data: {
+    scale: 14,
+    latitude: "",
+    longitude: "",
+  },
+
   onReady(e){
-    this.mapCtx.moveToLocation();
+    // this.mapCtx.moveToLocation();
   },
   onLoad() {
+    var that = this;
+    wx.getLocation({
+      success: function (res) {
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+        })
+      },
+    })
+
     this.mapCtx = wx.createMapContext('map', this);
   },
   go(){
